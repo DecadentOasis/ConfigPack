ce378f480f47313485db8fbbe7514e0c52d386c4
Matt Stone <matthew.d.stone@gmail.com>
2015-08-29 16:34:24 -0700

    Add palm tree art car configs

--- pixelpusher_configs/generate.py
+++ pixelpusher_configs/generate.py
@@ -77,3 +77,42 @@ gateway=192.168.111.1
 for i in range(1, 4):
     # IP addresses range from 192.168.111.151 to 192.168.111.153
     file("island-pusher-%02d-pixel.rc" % (i, ), "w").write(island_template % { "ipaddr": i+150 })
+
+palm_tree_art_car_template="""
+strip1=apa102
+strip2=apa102
+strip3=apa102
+strip4=apa102
+strip5=apa102
+strip6=apa102
+strip7=apa102
+strip8=apa102
+#swap=12345678
+pixels=48
+order1=rgb
+order2=rgb
+order3=rgb
+order4=rgb
+order5=rgb
+order6=rgb
+order7=rgb
+order8=rgb
+# strip colors, 1 through 8: red orange yellow green cyan blue purple white
+start1=ff0000
+start2=ff4000
+start3=ff4000
+start4=afaf00
+start5=00ff00
+start5=00afaf
+start6=0000ff
+start7=af00af
+start8=808080
+blank_strips_on_idle=1
+data_watchdog_time=300
+ether=192.168.0.%(ipaddr)s
+netmask=255.255.255.0
+gateway=192.168.0.1
+"""
+for i in range(0, 8):
+    # IP addresses range from 192.168.0.20 to 192.168.0.28
+    file("palm-tree-pusher-%02d-pixel.rc" % (i, ), "w").write(palm_tree_art_car_template % { "ipaddr": i+20 })
